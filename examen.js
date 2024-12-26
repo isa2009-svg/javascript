a9af06757ee8b2d9747b4855d8e0a41f89a291a2
ismail <azghj1679008766@gmail.com>
2024-12-26 20:00:03 +0600

    hehehehe

--- examen.js
+++
@@ -1,5 +0,0 @@
-function  example () {
-    let san = [10,20,30,40];
-    san.push(50);
-    console.log(san);   
-}
--- fail/yige.js
+++
@@ -1,4 +0,0 @@
-const holl =document.getElementById('holl')
-console.log(holl.innerHTML
-);
-holl.innerHTML = '<h1> byl men </h1>'
